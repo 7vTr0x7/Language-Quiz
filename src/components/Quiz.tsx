e9dc3f1c2288d1ea0dd7b09eb8c47f0c36e6fe55
7vTr0x7 <7vtrox7@gmail.com>
2024-03-27 15:53:46 +0530

    Quiz

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Quiz = () => {
+  const [result, setResult] = useState<string[]>([]);
+  const [count, setCount] = useState<number>(0);
+
+  const [ans, setAns] = useState<string>("");
+
+  const navigate = useNavigate();
+
+  const nextHandler = () => {
+    setResult((prev) => [...prev, ans]);
+    setCount((prev) => (prev += 1));
+  };
+
   return <div>Quiz</div>;
 };
 
