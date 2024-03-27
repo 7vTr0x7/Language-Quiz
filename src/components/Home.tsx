c50f1a231782cc8da07a6903edca392d419dcad8
7vTr0x7 <7vtrox7@gmail.com>
2024-03-27 15:03:26 +0530

    Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,45 @@
+import { Button, Container, Stack, Typography } from "@mui/material";
+
+const languages = [
+  {
+    name: "Japanese",
+    code: "ja",
+  },
+  {
+    name: "Hindi",
+    code: "hi",
+  },
+  {
+    name: "Spanish",
+    code: "es",
+  },
+  {
+    name: "French",
+    code: "fr",
+  },
+];
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container maxWidth={"sm"}>
+      <Typography variant="h4" p={"2rem"} textAlign={"center"}>
+        Welcome, Lets Begin Your Learning Journey
+      </Typography>
+
+      <Stack
+        direction={"row"}
+        spacing={"2rem"}
+        p={"2rem"}
+        alignItems={"center"}
+        justifyContent={"center"}>
+        {languages.map((item) => (
+          <Button key={item.code} variant="contained">
+            {item.name}
+          </Button>
+        ))}
+      </Stack>
+    </Container>
+  );
 };
 
 export default Home;
