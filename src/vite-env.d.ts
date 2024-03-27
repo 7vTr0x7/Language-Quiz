b0ab268c9f266d6ee0671c0136d2861c5c49f811
7vTr0x7 <7vtrox7@gmail.com>
2024-03-27 16:19:43 +0530

    state type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,16 @@
 /// <reference types="vite/client" />
 
 type LangType = "ja" | "hi" | "es" | "fr";
+
+interface IWord {
+  word: string;
+  meaning: string;
+  options: string[];
+}
+
+type StateType = {
+  loading: boolean;
+  result: string[];
+  words: IWord[];
+  error?: string;
+};
