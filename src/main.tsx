1efac9bc87a92232cae758548282802d53a4270b
7vTr0x7 <7vtrox7@gmail.com>
2024-03-27 16:11:44 +0530

    Redux Store

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import App from "./App.tsx";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
 import { theme } from "./utils/theme.ts";
+import { Provider } from "react-redux";
+import { store } from "./utils/redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
