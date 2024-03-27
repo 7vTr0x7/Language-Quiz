67ab3f31d41493354a31869851a788dc5156f158
7vTr0x7 <7vtrox7@gmail.com>
2024-03-27 14:37:41 +0530

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
