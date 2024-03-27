b19cf41d4b1604b08bc184d83ce71580073769d8
7vTr0x7 <7vtrox7@gmail.com>
2024-03-27 14:44:51 +0530

    lazy + routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("./components/Home"));
+const Learning = lazy(() => import("./components/Learning"));
+const Quiz = lazy(() => import("./components/Quiz"));
+const Result = lazy(() => import("./components/Result"));
+const Login = lazy(() => import("./components/Login"));
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route />
-      </Routes>
+
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/learn" element={<Learning />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
