df1c4d3d9c434a54bee477f69713020d51c936a3
7vTr0x7 <7vtrox7@gmail.com>
2024-03-27 14:52:28 +0530

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,28 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const style = {
+  color: "black",
+  margin: "0.5rem",
+  textDecoration: "none",
+};
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h5" mr={"auto"} textTransform={"uppercase"}>
+          LearnLang
+        </Typography>
+        <Link style={style} to={"/"}>
+          Home
+        </Link>
+        <Link style={style} to={"/login"}>
+          Login
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
